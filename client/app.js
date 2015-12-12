a1b9061f41bef7ad97c6904c66909af6bd9d3f97
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2015-12-12 02:33:42 +0000

    move Mongo collection statments that should be identical on both client and server to lib/collections.js

--- client/app.js
+++ client/app.js
@@ -1,9 +1,3 @@
-Matrices = new Mongo.Collection('matrices');
-Games = new Mongo.Collection('games');
-Experiments = new Mongo.Collection('experiments');
-Earnings = new Mongo.Collection('earnings');
-RollCall = new Mongo.Collection('rollcall');
-
 Tracker.autorun(function(){
     if (Meteor.userId()){
 	Meteor.subscribe('myuserdata');
