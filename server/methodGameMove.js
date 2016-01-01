49d4638181cc4d06118cdc68f62289bd6b66f712
Dr Paul Brewer <drpaulbrewer@gmail.com>
2016-01-01 03:33:31 -0500

    Remove short delay in updating moves, switch to asynchronous update call.

--- server/methodGameMove.js
+++ server/methodGameMove.js
@@ -19,34 +19,33 @@ Meteor.methods({
 	if (dim==='row'){
 	    if (game.rowUserId !== this.userId) return;
 	    if (game.rownames.length <= choiceIndex) return;
-	    Meteor.setTimeout(function(){
-		Games.update(gameId,
-			     {$set: {
-				 timeRow: ts,
-				 row: choiceIndex,
-			     },
-			      $push: {
-				  moves: [ts,0,choiceIndex]
-			      }
-			     });
-		updateLag = 0.8*updateLag+0.2*(+new Date()-ts);
-	    }, 20);
+	    Games.update(gameId,
+			 {$set: {
+			     timeRow: ts,
+			     row: choiceIndex,
+			 },
+			  $push: {
+			      moves: [ts,0,choiceIndex]
+			  }
+			 }, function(){
+			     updateLag = 0.9*updateLag+0.1*(+new Date()-ts);
+			 });
+	    
 	}
 	if (dim==='col'){
 	    if (game.colUserId !== this.userId) return;
 	    if (game.colnames.length <= choiceIndex) return;
-	    Meteor.setTimeout(function(){
-		Games.update(gameId,
-			     {$set: {
-				 timeCol: ts,
-				 col: choiceIndex,
-			     },
-			      $push: {
-				  moves: [ts, 1, choiceIndex]
-			      }
-			     });
-		updateLag = 0.8*updateLag+0.2*(+new Date()-ts);
-	    }, 20);
+	    Games.update(gameId,
+			 {$set: {
+			     timeCol: ts,
+			     col: choiceIndex,
+			 },
+			  $push: {
+			      moves: [ts, 1, choiceIndex]
+			  }
+			 }, function(){
+			     updateLag = 0.9*updateLag+0.1*(+new Date()-ts);
+			 });
 	}
     }
 });
