b484e163811337e9a188953831c0d073222298ec
Dr Paul Brewer <drpaulbrewer@gmail.com>
2016-01-10 11:27:58 -0500

    add user earnings to user record, and display on relevant admin screen

--- server/postGameEarnings.js
+++ server/postGameEarnings.js
@@ -1,8 +1,44 @@
 /* globals postGameEarnings:true */
 /* globals Games */
 
+var getAllEarnings = function(ts){
+    var i,l,id;
+    if (!ts) ts = +new Date();
+    var rowPipeline = [
+	{ $match: {timeEnds: {$lt: ts} } },
+	{ $project: { rowUserId:1, rowEarn: 1 } },
+	{ $sort: { rowUserId: 1 } },
+	{ $group: { _id: "$rowUserId", earn: { $sum: "$rowEarn" } } }
+    ];
+    var rowpay =  Games.aggregate(rowPipeline);
+    var colPipeline = [
+	{ $match: {timeEnds: {$lt: ts} } },
+	{ $project: { colUserId:1, colEarn: 1 } },
+	{ $sort: { colUserId: 1 } },
+	{ $group: { _id: "$colUserId", earn: { $sum: "$colEarn" } } }
+    ];
+    var colpay = Games.aggregate(colPipeline);
+    var pay = {};
+    for(i=0,l=rowpay.length; i<l; ++i){
+	id = rowpay[i]._id;
+	if (pay[id])
+	    pay[id] += rowpay[i].earn;
+	else
+	    pay[id] = rowpay[i].earn;
+    }
+    for(i=0,l=colpay.length; i<l; ++i){
+	id = colpay[i]._id;
+	if (pay[id])
+	    pay[id] += colpay[i].earn;
+	else
+	    pay[id] = colpay[i].earn;
+    }
+    return pay;
+};
+
 postGameEarnings = function(){
     "use strict";
+    var id;
     var theseGames = Games.find({
 	timeEnds: {$lt: +new Date()},
 	aborted: {$exists: false},
@@ -23,5 +59,10 @@ postGameEarnings = function(){
 	    rowEarn: thisGame.rowMatrix[thisGame.row][thisGame.col],
 	    colEarn: thisGame.colMatrix[thisGame.row][thisGame.col]
 	}});
-    }, function(){});
+    });
+    var pay = getAllEarnings(+new Date());
+    for (id in pay){
+	if (pay.hasOwnProperty(id))
+	    Meteor.users.update(id, {$set: {earnings: pay[id]} }, function(){} );
+    }
 };
